268453ec422ef1604177bc6e2b9c5bf258f5b81c
lvxin0315@163.com <lvxin0315@163.com>
2018-04-19 13:17:14 +0800

    跨域

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	_ "beegoDemo/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 )
 
 func main() {
+	filter := cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	})
+	beego.InsertFilter("*", beego.BeforeRouter, filter)
 	beego.Run()
 }
 
