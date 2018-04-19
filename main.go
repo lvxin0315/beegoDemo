c807baf34cdd9864d8a22c057ffaf72b1e761a68
lvxin0315@163.com <lvxin0315@163.com>
2018-04-19 14:23:29 +0800

    日志级别

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	_ "beegoDemo/routers"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/plugins/cors"
+	"github.com/astaxie/beego/logs"
 )
 
 func main() {
+	//设置跨域
 	filter := cors.Allow(&cors.Options{
 		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -14,6 +16,9 @@ func main() {
 		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
 		AllowCredentials: true,
 	})
+	//日志级别
+	logs.SetLogger(logs.AdapterConsole, `{"level":3}`)
+
 	beego.InsertFilter("*", beego.BeforeRouter, filter)
 	beego.Run()
 }
